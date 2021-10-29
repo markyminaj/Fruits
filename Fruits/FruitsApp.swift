5461d2202e5191aa74e84e36bae715b04bdc4db0
Mark <menm619@gmail.com>
2021-10-29 16:21:50 -0700

    Initial Commit

---
+++ Fruits/FruitsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FruitsApp.swift
+//  Fruits
+//
+//  Created by Mark Martin on 10/29/21.
+//
+
+import SwiftUI
+
+@main
+struct FruitsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
